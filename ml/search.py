0badc6173eb3452dd061cdc9ab1287fca9d74968
Nikita <nikiton1545@mail.ru>
2026-07-04 03:58:17 +0300

    micro-edit search

--- ml/search.py
+++ ml/search.py
@@ -20,7 +20,6 @@ class Search(NerPipeline):
         """
         RUSSIA_LOCATIONS = {"россия", "рф", "сибирь", "кузбасс", "урал", "якутия", "москва", "спб", "норильск", "кольский"}
 
-        # Прогоняем текст через НАШУ кастомную NER-модель (с EntityRuler)
         doc = super().ner_for_search(raw_query)
         
         filters = {
@@ -30,16 +29,16 @@ class Search(NerPipeline):
         
         words_to_remove = set()
 
-        # 1. География: простая проверка по ключевым словам прямо в строке
+
         query_lower = raw_query.lower()
         if any(loc in query_lower for loc in RUSSIA_LOCATIONS) or "россий" in query_lower:
             filters["geo"] = "Россия"
         elif "зарубеж" in query_lower or "иностран" in query_lower:
             filters["geo"] = "Зарубежье"
 
-        # 2. Бежим по сущностям и собираем числовые фильтры через готовые лейблы
+
         for ent in doc.ents:
-            # Вырезаем гео-сущности из базовой модели, чтобы они не шумели в эмбеддинге
+
             if ent.label_ in ["GPE", "LOC"]:
                 words_to_remove.add(ent.text)
                 
@@ -60,19 +59,18 @@ class Search(NerPipeline):
                         "unit": prop.get("unit")
                     })
 
-        # 3. Очищаем текст от найденных параметров и локаций
         clean_text = raw_query
         for word in words_to_remove:
             clean_text = clean_text.replace(word, " ")
         
-        # Вычищаем мусорные знаки, которые могли остаться висеть в воздухе
+
         for op in ["<=", ">=", "<", ">", "=", "–", "-"]:
             clean_text = clean_text.replace(op, " ")
             
-        # Убираем двойные пробелы
+
         clean_text = " ".join(clean_text.split())
 
-        # 4. Генерируем вектор только из смысловой части (например, "хлорное выщелачивание файнштейн")
+        
         query_vector = self.embedder.text_to_vector(clean_text).tolist()
 
         return {
